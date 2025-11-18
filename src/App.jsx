243f30164f5dc7d413e8db6e8e99be88360982b3
MoDevIO <momo.tiltis@gmail.com>
2025-11-18 17:32:20 +0100

    initinal commit with color theme

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>TITLE</h1>
+      <button
+        style={{ boxShadow: "var(--shadow)", width: "100px", height: "40px" }}
+      >
+        BUTTON
+      </button>
+    </>
+  );
+}
+
+export default App;
